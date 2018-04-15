3b75bc925dd48dd22c3fa956d7aa23c5979de35e
EYYY <38400483+EYYY@users.noreply.github.com>
2018-04-15 23:16:53 +0900

    ''
    how to upload

---
+++ ch03/mnist_test97.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+from dataset.mnist import load_mnist
+#from PIL import Image
+
+#import numpy as np
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
+
+
+'''
+img = x_train[0]
+label = t_train[0]
+print(label)  # 5
+
+print(img.shape)  # (784,)
+img = img.reshape(28, 28)  # 형상을 원래 이미지의 크기로 변형
+print(img.shape)  # (28, 28)
+
+img_show(img)
+'''
